80ba0f2d1ed722ab4bb7644c47351a61abc4475e
Jack <guitarpoet@gmail.com>
2012-10-29 14:32:24 +0800

    Added make to make life easier.

--- tools.js.runner.application/src/main/resources/scripts/init.js
+++ tools.js.runner.application/src/main/resources/scripts/init.js
@@ -1,15 +1,32 @@
-print=function(x) {java.lang.System.out.print(x)};
-println=function(x) {java.lang.System.out.println(x);};
-printf=function(){utils.printf.apply(utils, arguments)};
+print = function(x) {
+	java.lang.System.out.print(x)
+};
+println = function(x) {
+	java.lang.System.out.println(x);
+};
+printf = function() {
+	utils.printf.apply(utils, arguments)
+};
 console = {
-		log: function() {logger.info.apply(logger, arguments);},
-		info: function() {logger.info.apply(logger, arguments);},
-		warn: function() {logger.warn.apply(logger, arguments);},
-		error: function() {logger.error.apply(logger, arguments);}
+	log : function() {
+		logger.info.apply(logger, arguments);
+	},
+	info : function() {
+		logger.info.apply(logger, arguments);
+	},
+	warn : function() {
+		logger.warn.apply(logger, arguments);
+	},
+	error : function() {
+		logger.error.apply(logger, arguments);
+	}
 }
 String.prototype.split = function(sep) {
 	return sutils.split(this, sep);
 }
 String.prototype.isBlank = function() {
 	return sutils.isBlank(this);
+}
+String.prototype.capitalize = function() {
+	return sutils.capitalize(this);
 }
--- tools.js.runner.application/src/main/scripts/simple.js
+++ tools.js.runner.application/src/main/scripts/simple.js
@@ -1,5 +1,8 @@
 utils.require('underscore');
 
+console.log("test {}", "adsf");
+logger.info("test {}", "asdf");
+console.log("test {}", "asdf".capitalize());
 console.log("".isBlank());
 printf("Time is {0}", new Date().toString());
 console.log("a,b,c,d".split(","));
