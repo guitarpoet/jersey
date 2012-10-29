d49ad6eb62d241baa266a79dc383c7a7d940a087
Jack <guitarpoet@gmail.com>
2012-10-29 15:28:21 +0800

    Added chinese test.

--- tools.js.runner.application/src/main/scripts/simple.js
+++ tools.js.runner.application/src/main/scripts/simple.js
@@ -3,6 +3,7 @@ utils.require('underscore');
 console.log(java.lang.System.getProperties());
 console.log("test {}", "adsf");
 logger.info("test {}", "asdf");
+logger.info("test {}", "中文");
 console.log("test {}", "asdf".capitalize());
 console.log("".isBlank());
 printf("Time is {0}", new Date().toString());
